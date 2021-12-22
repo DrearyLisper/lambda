1b704f2ca6df6efc741c6b31c1a94d99b63bd7e9
Dreary Lisper <drearylisper@gmail.com>
2021-12-22 00:07:59 +0000

    Base parsing.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,36 @@
 module Main where
 
-main = print "Hello, world"
+import Data.List
+import Control.Monad (forM_)
+
+data Expression = Name String | Function Expression Expression | Application Expression Expression deriving Show
+
+parseName :: String -> (Expression, String)
+parseName xs = parseName' xs []
+  where
+    parseName' [] name = (Name $ reverse name, [])
+    parseName' (x:xs) name | x `elem` ['.', '\\', '(', ')', ' '] = (Name $ reverse name, x:xs)
+                           | otherwise                      = parseName' xs (x:name)
+
+parseFunction :: String -> (Expression, String)
+parseFunction xs = (Function argumentName functionBody, rest')
+  where
+    (argumentName, rest) = parseName xs
+    (functionBody, rest') = parseExpression $ tail rest
+
+parseApplication :: String -> (Expression, String)
+parseApplication xs = (Application functionExpression argumentExpression, tail rest)
+  where
+    (functionExpression, rest) = parseExpression xs
+    (argumentExpression, rest') = parseExpression $ tail rest
+
+parseExpression :: String -> (Expression, String)
+parseExpression [] = error "Empty string"
+parseExpression (x:xs) | x == '\\' = parseFunction xs
+                       | x == '('  = parseApplication xs
+                       | otherwise = parseName (x:xs)
+
+lambdas :: [String]
+lambdas = ["\\x.x", "\\first.\\second.first", "\\s.(s s)", "\\func.\\arg.(func arg)"]
+
+main = forM_ (map (fst.parseExpression) lambdas) print
