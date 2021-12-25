f4cb696f5a83890dad6107e92835e1f44d41e9ef
Dreary Lisper <drearylisper@gmail.com>
2021-12-25 21:18:22 +0000

    Alpha conversion during Beta.

--- lib/Lambda/Evaluation.hs
+++ lib/Lambda/Evaluation.hs
@@ -1,31 +1,45 @@
+{-# LANGUAGE RecordWildCards #-}
 module Lambda.Evaluation where
 
 import Lambda.Types
+import Debug.Trace
 
 import qualified Data.Set as Set
+import Data.Maybe (fromMaybe)
+
+data BetaContext = BetaContext {argumentName :: String, argumentValue :: Expression, bond :: Maybe String, replacementName :: Maybe String}
 
 beta :: Expression -> Expression -> Expression
-beta (Function (Name argumentName) body) argumentValue = beta' body argumentName argumentValue
+beta (Function (Name argumentName) body) argumentValue = beta' body (BetaContext argumentName argumentValue Nothing Nothing)
   where
-    beta' :: Expression -> String -> Expression -> Expression
-    beta' (Name name) argumentName argumentValue | name == argumentName = argumentValue
-                                                 | otherwise            = Name name
+    freeArgNames = free argumentValue
 
-    beta' (Function (Name innerName) innerBody) argumentName argumentValue
-      | innerName /= argumentName = Function (Name innerName) (beta' innerBody argumentName argumentValue)
+    beta' :: Expression -> BetaContext -> Expression
+    beta' (Name name) BetaContext{..} | name == argumentName = argumentValue
+                                      | Just name == bond    = Name $ fromMaybe name replacementName
+                                      | otherwise            = Name name
+
+    beta' (Application function argument) betaContext = Application (beta' function betaContext) (beta' argument betaContext)
+
+    beta' (Function (Name innerName) innerBody) BetaContext{..}
+      | innerName /= argumentName = Function (Name name) (beta' innerBody BetaContext{bond = Just innerName, replacementName = Just name, ..})
       | otherwise = Function (Name innerName) innerBody
+      where
+        name = if innerName `Set.member` freeArgNames
+                then "r-" ++ innerName
+                else innerName
 
-    beta' (Application func arg) argumentName argumentValue = Application (beta' func argumentName argumentValue) (beta' arg argumentName argumentValue)
+
 beta _ _ = error "Can't substitute into non-function expression"
 
-free :: Expression -> [String]
+free :: Expression -> Set.Set String
 free expression = free' expression Set.empty
   where
-    free' :: Expression -> Set.Set String -> [String]
-    free' (Name a) bound = [a | not (a `Set.member` bound)]
+    free' :: Expression -> Set.Set String -> Set.Set String
+    free' (Name a) bound = Set.fromList [a | not (a `Set.member` bound)]
     free' (Function (Name argument) body) bound = free' body (argument `Set.insert` bound)
     free' (Function argument body) bound = error "Function argument can be Name String only"
-    free' (Application function argument) bound = free' function bound ++ free' argument bound
+    free' (Application function argument) bound = free' function bound `Set.union` free' argument bound
 
 eval :: Expression -> Expression
 eval (Name name) = Name name
