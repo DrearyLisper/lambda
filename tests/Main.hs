81a37ce0aaeee4a85bab6494da6eeb9a0a667cdd
Dreary Lisper <drearylisper@gmail.com>
2021-12-26 00:31:33 +0000

    More tests for Parsing.hs

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,8 +5,21 @@ import Test.Hspec
 import Lambda.Parsing
 import Lambda.Types
 
+
+-- (\x.(x x) (\f.(f (f y)) \x.x))
+
 main :: IO ()
 main = hspec $ do
   describe "Lambda.Parsing.parseExpression" $ do
     it "can parse names" $ do
       parseExpression "x" `shouldBe` (Name "x", "")
+      parseExpression "1" `shouldBe` (Name "1", "")
+    it "can parse applications" $ do
+      parseExpression "(x x)" `shouldBe` ((Application (Name "x") (Name "x")), "")
+      parseExpression "(\\x.x x)" `shouldBe` ((Application
+                                              (Function
+                                               (Name "x")
+                                               (Name "x"))
+                                              (Name "x")), "")
+    it "can parse functions" $ do
+      parseExpression "\\x.x" `shouldBe` ((Function (Name "x") (Name "x")), "")
